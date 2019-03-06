b76f420fcf1f505dd2b9d3bd92061a16b9d57257
Nellie Wangari <Nelliewangari11@gmail.com>
2019-03-06 15:51:28 +0300

    add README

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-{Quiz-Board}
-{A JavaScript quiz for students and it gives results of the test after the students has it.}, {24/2/2019}
-By {Nellie}
-
- list setup instructions
- The student should do the test and submit the answers to get the results
-
-The application is:
- simple
-easy-to-understand
-The project uses languages like HTML, CSS,JavaScript and bootstrap
-
-Support and contact details
+# Quiz-Board
+#### A JavaScript quiz for students and it gives results of the test after the students has it.}, {24/2/2019}
+#### By {Nellie}
+## Description
+The student answers all the questions and the end of the quiz he/she is able to get his/hers results.
+## Behavior Driven Development
+| Input                | Behavior                        | Output                             |
+|----------------------|---------------------------------|------------------------------------|
+| index.HTML code      |       Loads the questions       |  Displays the questions to the use |
+| Submit button        |      load the results                           |  show the  results                |
+## User Stories
+* As a user, I should see multiple choice questions when the page loads.
+* As a user, I want to select the correct answer.
+* As a user, I want to submit the answers when done.
+* As a user, I should see the results.
+* As a user, I should be able to retake the quiz if I fail.
+## Setup/Installation Requirements
+* A computer with internet access is required.
+* A browser, preferably Google Chrome, should be installed.
+* Access the site in the link provided in the project description.
+## Known Bugs
+No known bugs at the time.
+## Technologies Used
+The project uses languages like HTML, CSS,JavaScript and bootstrap.
+## Support and contact details
 In case you need help to understand or add something to the application you can email me at: Nelliewangari11@gmail.com
 
-License
+### License
+*License
 MIT License
 
 Copyright (c) [2019] [Nellie Wangari]
--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,62 @@
-// scripts here:
 
-	function submitQuiz() {
-		console.log('submitted');
+		
 
-	// get each answer score
-		function answerScore (qName) {
-			var radiosNo = document.getElementsByName(qName);
 
-			for (var i = 0, length = radiosNo.length; i < length; i++) {
-   				if (radiosNo[i].checked) {
-			// do something with radiosNo
-					var answerValue = Number(radiosNo[i].value);
-				}
-			}
-			// change NaNs to zero
-			if (isNaN(answerValue)) {
-				answerValue = 0;
-			}
-			return answerValue;
-		}
 
-	// calc score with answerScore function
-		var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
-		console.log("CalcScore: " + calcScore); // it works!
+//function for adding total scores.
+var addResults = function(results){
+  //check for NaN values, i.e, non-answered questions
+  var finalResults=[];
+  results.forEach(function(result){
+    if(isNaN(result)){
+      finalResults.push(0);
+    }else{
+      finalResults.push(result);
+    }
+  });
+  var totalScore = 0;
+  for(var i = 0; i<finalResults.length; i++){
+    totalScore += finalResults[i];
+  }
+  return totalScore;
+};
 
-	// function to return correct answer string
-		function correctAnswer (correctStringNo, qNumber) {
-			console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-			return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
-				(document.getElementById(correctStringNo).innerHTML) + "</strong>");
-			}
+var gradeChecker =function(score){
+  var gradeChecker;
+  if(score>=80){
+    gradeChecker ="Congratulations!! You have passed Excellently.";
+  }else if(score>=50){
+    gradeChecker="Good job! You have passed fairly.";
+  }else{
+    gradeChecker = "You have failed. Please try again.";
+  }
+  return gradeChecker;
+};
+$(document).ready(function(){
 
-	// print correct answers only if wrong (calls correctAnswer function)
-		if (answerScore('q1') === 0) {
-			document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-		}
-		if (answerScore('q2') === 0) {
-			document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-		}
-		if (answerScore('q3') === 0) {
-			document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
-		}
-		if (answerScore('q4') === 0) {
-			document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
-		}
+  $("#form form").submit(function(event){
+    event.preventDefault();
+    //array for storing results
+    var results =[];
+    // for loop for entering values from the form to the array
+    for(var index =0; index<10; index++){
+      results[index]= parseInt($("input:radio[name=question"+(index+1)+"]:checked").val());
+    }
 
-	// calculate "possible score" integer
-		var questionCountArray = document.getElementsByClassName('question');
+     var totalScore = addResults(results);
+     var grade =gradeChecker(totalScore);
+     $("#scoreResult").text(totalScore);
+     $("#extraInfo").text(grade);
 
-		var questionCounter = 0;
-		for (var i = 0, length = questionCountArray.length; i < length; i++) {
-			questionCounter++;
-		}
+     $("#scoreboard").show();
 
-	// show score as "score/possible score"
-		var showScore = "Your Score: " + calcScore +"/" + questionCounter;
-	// if 4/4, "perfect score!"
-		if (calcScore === questionCounter) {
-			showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
-		};
-		document.getElementById('userScore').innerHTML = showScore;
-	}
+     $("#quiz").hide();
 
-$(document).ready(function() {
 
-	$('#submitButton').click(function() {
-		$(this).addClass('hide');
-	});
+  });
+  $(".quizbtn").click(function(){
+    $("#quiz").show();
+  });
+
 
 });
